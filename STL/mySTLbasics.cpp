e42c87cd8eec84f0f4c4b8ea5048d2939935fa05
Prateek Rohilla <prateek98rohilla@gmail.com>
2023-04-08 17:02:00 +0530

    common STL implementations

---
+++ STL/mySTLbasics.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void testfun(){
+	
+}
+
+main(){
+	
+	
+	
+	
+	/*
+	// swap() function
+	// - works with any data type
+	
+	int a = 10; 
+    int b = 20; 
+    
+	cout << "Value of a before: " << a << endl; 
+    cout << "Value of b before: " << b << endl; 
+  
+    swap(a, b); 
+    
+    cout << "Value of a now: " << a << endl; 
+    cout << "Value of b now: " << b << endl;
+	
+	//swapping string
+	string a = "Geeks"; 
+    string b = "function"; 
+    cout << "Value of a before: " << a << endl; 
+    cout << "Value of b before: " << b << endl; 
+    swap(a, b); 
+    cout << "Value of a now: " << a << endl; 
+    cout << "Value of b now: " << b << endl;
+	
+	*/
+	
+	
+	
+	
+	/*
+	// sort()
+	// -- include algorithm
+	
+	int a[10]= {1, 5, 8, 9, 6, 7, 3, 4, 2, 0}; 
+    cout << "\n The array before sorting is : ";
+	
+	for(int i = 0; i < 10; ++i) 
+        cout << a[i] << " ";
+        
+    sort(a, a+10);
+    
+    //for sorting in reverse
+    //sort(a, a+10,greater<int>());
+    
+    cout << "\n\n The array after sorting is : ";
+	for(int i = 0; i < 10; ++i) 
+        cout << a[i] << " ";
+	*/
+	
+	
+	
+	
+	/*
+	// binary_search(startaddress, endaddress, valuetofind)
+	//returns boolean true if the element is present in the container, else returns false
+	
+	int arr[] = {10, 15, 20, 25, 30, 35}; 
+	
+	if (binary_search(arr, arr+6, 15)) 
+       cout << "15 exists in vector"; 
+    else 
+       cout << "15 does not exist"; 
+       
+    cout << endl; 
+    */
+	
+	
+	
+	
+	/*
+	//  lower_bound(start_ptr, end_ptr, num)
+	// Returns pointer to “position of num” if container contains 1 occurrence of num
+	// Returns pointer to “first position of num” if container contains multiple occurrence of num
+	// Returns pointer to “position of next higher number than num” if container does not contain occurrence of num
+	// Subtracting the pointer to 1st position i.e “arr” returns the actual index
+	int arr[] = {10, 15, 20, 25, 30, 35}; 
+	
+	cout << "The position of 20 using lower_bound "
+            " (in single occurrence case) : "; 
+    cout << lower_bound(arr, arr+6, 20) - arr;
+	*/
+	
+	
+	
+	
+	/*
+	//gcd  -- works only in GCC
+	cout<<"GCD of 18 & 45 is " << __gcd(18,45);
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
